e423f0002f3348dcb73ae2cf91dc91bd3d850b17
Fabian *-* <nelsonfabiancs8@gmail.com>
2023-10-17 18:35:04 -0500

    database connection added

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from database import get_connection, release_connection
 
 app = FastAPI()
 
+def get_db():
+    db = None
+    try:
+        cursor = get_connection()
+        yield cursor
+    finally:
+        if db:
+            release_connection(db)
+
 @app.get("/")
-async def root():
+async def read_root(db=Depends(get_db)):
+    # Tu código que usa la conexión a la base de datos
+    db.execute("SELECT ...")  # Ejemplo
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
