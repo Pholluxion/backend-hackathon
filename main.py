074bc7fd50dc616f00334dcb3de5e5e8de3a5281
Fabian *-* <nelsonfabiancs8@gmail.com>
2023-10-17 18:35:37 -0500

    added a comment in db.execute

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ def get_db():
 @app.get("/")
 async def read_root(db=Depends(get_db)):
     # Tu código que usa la conexión a la base de datos
-    db.execute("SELECT ...")  # Ejemplo
+    #db.execute("SELECT ...")  # Ejemplo
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
